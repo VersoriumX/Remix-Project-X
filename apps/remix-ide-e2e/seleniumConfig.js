898ea74d141fb0bd0b222c71046061c7f3d09720
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2024-12-31 08:43:13 -0500

    Update seleniumConfig.js

--- apps/remix-ide-e2e/seleniumConfig.js
+++ apps/remix-ide-e2e/seleniumConfig.js
@@ -8,5 +8,62 @@ module.exports = {
       arch: process.arch,
       baseURL: 'https://chromedriver.storage.googleapis.com'
     }
+  },
+  ethereum: {
+    layer1: {
+      hardhat: {
+        version: '2.9.0', // Specify the Hardhat version
+        network: 'mainnet', // Mainnet for Layer 1
+        settings: {
+          gas: 'auto', // Automatically estimate gas
+          gasPrice: 'auto', // Automatically estimate gas price
+        }
+      },
+      foundry: {
+        version: '0.2.0', // Specify the Foundry version
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    },
+    layer2: {
+      arbitrum: {
+        hardhat: {
+          network: 'arbitrum', // Arbitrum network for Layer 2
+        },
+        foundry: {
+          network: 'arbitrum', // Arbitrum network for Foundry
+        }
+      },
+      polygon: {
+        hardhat: {
+          network: 'polygon', // Polygon network for Layer 2
+        },
+        foundry: {
+          network: 'polygon', // Polygon network for Foundry
+        }
+      }
+    },
+    layer3: {
+      optimism: {
+        hardhat: {
+          network: 'optimism', // Optimism network for Layer 3
+        },
+        foundry: {
+          network: 'optimism', // Optimism network for Foundry
+        }
+      },
+      starknet: {
+        hardhat: {
+          network: 'starknet', // Starknet network for Layer 3
+        },
+        foundry: {
+          network: 'starknet', // Starknet network for Foundry
+        }
+      }
+    }
   }
 }
